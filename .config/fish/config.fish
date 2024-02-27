617181b08aa76289b51ce98e23ec978f51eea0a2
Davide Nale <davide.nale.99@gmail.com>
2024-02-27 15:44:39 +0100

    Small changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,8 +2,12 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# mambaforge
-source "/opt/mambaforge/etc/fish/conf.d/conda.fish"
+# >>> mamba initialize >>>
+# !! Contents within this block are managed by 'mamba init' !!
+set -gx MAMBA_EXE "/usr/bin/micromamba"
+set -gx MAMBA_ROOT_PREFIX "/home/davide/micromamba"
+$MAMBA_EXE shell hook --shell fish --root-prefix $MAMBA_ROOT_PREFIX | source
+# <<< mamba initialize <<<
 
 # custom additions
 set fish_greeting
@@ -14,7 +18,10 @@ alias dots='git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
 
 # aliases
 alias hx=helix
+alias vim=nvim
 alias py=python
 alias gg=git-graph
 alias ls=exa
+alias mamba=micromamba
 alias cc='cd (fd -H -d 5 -c always --type directory | fzf --ansi --layout reverse --tiebreak length,chunk)'
+
