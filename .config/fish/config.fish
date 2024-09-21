77ca360327220001175eecee4cedaa065120614f
Davide Nale <davide.nale.99@gmail.com>
2024-09-21 12:05:15 +0200

    Removed mamba

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,13 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# >>> mamba initialize >>>
-# !! Contents within this block are managed by 'mamba init' !!
-set -gx MAMBA_EXE "/usr/bin/micromamba"
-set -gx MAMBA_ROOT_PREFIX "/home/davide/micromamba"
-$MAMBA_EXE shell hook --shell fish --root-prefix $MAMBA_ROOT_PREFIX | source
-# <<< mamba initialize <<<
-
 # custom additions
 set fish_greeting
 starship init fish | source
@@ -24,4 +17,3 @@ alias gg=git-graph
 alias ls=exa
 alias mamba=micromamba
 alias cc='cd (fd -H -d 5 -c always --type directory | fzf --ansi --layout reverse --tiebreak length,chunk)'
-
